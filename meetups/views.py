7a31d239afb5800aded957572d1f585f1225a2b8
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2021-09-18 20:51:04 +0530

    djngo loops

--- meetups/views.py
+++ meetups/views.py
@@ -3,4 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,'meetups/index.html')
+    meetups = [
+            {   
+                'title':'the first meetups',
+                'location':'colombo',
+                'slug':'first-meetup'
+            },
+            {
+                'title':'second meetup',
+                'location':'galle',
+                'slug':'second-meetup'
+            }
+                ]
+    return render(request,'meetups/index.html',{
+        'show_meetups':True,
+        'meetups':meetups
+    })
