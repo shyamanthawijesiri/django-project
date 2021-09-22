3a6fa22a7f6397158e16920a05cc6c19bbe2796e
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2021-09-22 19:35:38 +0530

    acc and dynamic url

--- meetups/urls.py
+++ meetups/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('meetups/',views.index)
+    path('meetups/',views.index),
+    path('meetups/<slug:meetup_slug>/',views.meeup_details)
     
     ]
--- meetups/views.py
+++ meetups/views.py
@@ -19,3 +19,13 @@ def index(request):
         'show_meetups':True,
         'meetups':meetups
     })
+
+def meeup_details(request,meetup_slug):
+    seleted_meetup = {
+                        'title':'first meetup',
+                        'description':'this is the first meetups'
+                    }
+    return render(request,'meetups/meetup-details.html',{
+        'meetup_title':seleted_meetup['title'],
+        'meetup_description':seleted_meetup['description']
+    })
