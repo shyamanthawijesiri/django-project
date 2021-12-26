1c6f5e8630b47ef4ec5fe1d0a992c567ce1cfdea
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2021-12-26 12:04:36 +0530

    dynamic url

--- meetups/urls.py
+++ meetups/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('meetups/',views.index),
-    path('meetups/<slug:meetup_slug>/',views.meeup_details)
+    path('meetups/',views.index, name='all-meetups'),
+    path('meetups/<slug:meetup_slug>/',views.meeup_details, name='meetup-detail')
     
     ]
