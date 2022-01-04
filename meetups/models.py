223ab5ee085670774b03d51668ccf4fd193a4931
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2022-01-04 18:48:01 +0530

    image uploading

--- meetups/models.py
+++ meetups/models.py
@@ -5,4 +5,5 @@ from django.db import models
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    description = models.TextField()
+    description = models.TextField() 
+    image = models.ImageField(upload_to='images')
